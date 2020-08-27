5ce83bca00780ac227f6d80e5ff3391f2278e8e0
Kevin Nacario <hitchcliff1@gmail.com>
2020-08-27 18:00:03 +0800

    Updates

---
+++ precache-manifest.78b583934ad36686bb392f62ca03a1f6.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bc35e6401dce7af1555d917342263ab",
+    "url": "/onepage-studio/index.html"
+  },
+  {
+    "revision": "e0f4931b47eed7565359",
+    "url": "/onepage-studio/static/css/2.62313666.chunk.css"
+  },
+  {
+    "revision": "aba296f98b1eabf456fd",
+    "url": "/onepage-studio/static/css/main.4cd069c5.chunk.css"
+  },
+  {
+    "revision": "e0f4931b47eed7565359",
+    "url": "/onepage-studio/static/js/2.c7cf9154.chunk.js"
+  },
+  {
+    "revision": "6ef718eeb39c03c0fcdab1a60b0c6b00",
+    "url": "/onepage-studio/static/js/2.c7cf9154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aba296f98b1eabf456fd",
+    "url": "/onepage-studio/static/js/main.6e72f02f.chunk.js"
+  },
+  {
+    "revision": "6e88f601e1473f14384a",
+    "url": "/onepage-studio/static/js/runtime-main.9825c6d1.js"
+  },
+  {
+    "revision": "7e61138ed7c1ec3493d610f73c2990fe",
+    "url": "/onepage-studio/static/media/banner1.7e61138e.jpg"
+  },
+  {
+    "revision": "84619eee8d607e3cb74bf7b0cbbd5502",
+    "url": "/onepage-studio/static/media/counter.84619eee.jpg"
+  },
+  {
+    "revision": "c3d08d2ee2153eb3c5b39859dbe9f68a",
+    "url": "/onepage-studio/static/media/footer.c3d08d2e.jpg"
+  },
+  {
+    "revision": "65cc6a25985381b9e4dfeaf085814870",
+    "url": "/onepage-studio/static/media/quote.65cc6a25.jpg"
+  },
+  {
+    "revision": "2182330384ebbad826821fdf8e84efe5",
+    "url": "/onepage-studio/static/media/react-bootstrap-carousel.21823303.eot"
+  },
+  {
+    "revision": "2fe6d179ea5aa1ceab26113e77e13da4",
+    "url": "/onepage-studio/static/media/react-bootstrap-carousel.2fe6d179.svg"
+  },
+  {
+    "revision": "c746c4882df00af3b0a7a3b1616bf295",
+    "url": "/onepage-studio/static/media/react-bootstrap-carousel.c746c488.woff"
+  },
+  {
+    "revision": "f7448c0408e7a78012a82def0fb9e2fe",
+    "url": "/onepage-studio/static/media/react-bootstrap-carousel.f7448c04.ttf"
+  },
+  {
+    "revision": "fac23fc8be1dced95791a635c0100676",
+    "url": "/onepage-studio/static/media/services1.fac23fc8.png"
+  },
+  {
+    "revision": "45e62c7bb3649fd3e9ecc5d74b5e2434",
+    "url": "/onepage-studio/static/media/services2.45e62c7b.jpg"
+  },
+  {
+    "revision": "2297a81a8ad8198bf18f87a66413e787",
+    "url": "/onepage-studio/static/media/team1.2297a81a.jpg"
+  },
+  {
+    "revision": "202d9de185cbefd10ee5e467737b913d",
+    "url": "/onepage-studio/static/media/team2.202d9de1.jpg"
+  },
+  {
+    "revision": "d7ab802da04c0f72d00925686d7d7063",
+    "url": "/onepage-studio/static/media/team3.d7ab802d.jpg"
+  },
+  {
+    "revision": "25027d600d4d9d0b94d396e550de0cc2",
+    "url": "/onepage-studio/static/media/testimonial1.25027d60.jpg"
+  },
+  {
+    "revision": "a25c730f94ed03a80e20c7e964c05963",
+    "url": "/onepage-studio/static/media/testimonial2.a25c730f.jpg"
+  },
+  {
+    "revision": "e6e89ddc2379f742917128be3f9d69e7",
+    "url": "/onepage-studio/static/media/vision-mission.e6e89ddc.jpg"
+  },
+  {
+    "revision": "d944309d9ee8acee46c4dda40f4c8953",
+    "url": "/onepage-studio/static/media/work1.d944309d.jpg"
+  },
+  {
+    "revision": "907158c81f268c1bfd68d93a17d3072d",
+    "url": "/onepage-studio/static/media/work2.907158c8.jpg"
+  },
+  {
+    "revision": "75d621773e9789fd65a98e4863a2fc1d",
+    "url": "/onepage-studio/static/media/work3.75d62177.jpg"
+  }
+]);
